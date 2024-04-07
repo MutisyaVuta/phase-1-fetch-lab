459a523cb2723d292b7399a5c0a023ebffba8e59
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-04-07 22:26:13 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+      .then(function renderBooks (books) {
+        return books.json();
+      })
+      .then(function (data) {
+        console.log(data)
+      })
+
   
-}
+  
+    }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
